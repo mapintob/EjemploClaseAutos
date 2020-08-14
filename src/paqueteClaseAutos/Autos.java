0e9333862f5643629c7ad41a869cf1b9e8bbd18d
miguel <mpinto@academicos.uta.cl>
2020-08-13 21:05:40 -0300

    commit inicial

---
+++ src/paqueteClaseAutos/Autos.java
@@ -0,0 +1,44 @@
+package paqueteClaseAutos;
+
+public class Autos {
+    //atributos
+    String color;
+    int altura;
+    String marca;
+    String modelo;
+    String traccion;
+    String motor;
+    int kilometraje;
+    int marchaActual;
+    
+    //metodos
+    void acelerar(){
+        System.out.println("auto acelerando..");
+    }
+    
+    void frenar(){
+        System.out.println("auto frenando..");
+    }
+    
+    void encender(String nombreAuto){
+        System.out.println("auto encendiendo.."+nombreAuto);        
+    }
+    
+    String cambiarMarcha(int marcha){        
+        return ("Auto en marcha: "+marcha);
+    }
+    
+    public static void main(String [] args){
+        Autos autito = new Autos();
+        autito.color="rojo";
+        
+        System.out.println("Color del auto: "+autito.color);
+        autito.color="azul";
+         
+        System.out.println("Color del auto: "+autito.color);
+        autito.encender("el lambo...");
+        autito.acelerar();
+        autito.frenar();
+     
+    }
+}
